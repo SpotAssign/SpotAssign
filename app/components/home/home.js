b8d3c645c455a798197e7b638861077d50555525
Cameron Nuckols <camnuckols@gmail.com>
2016-07-26 14:07:48 -0500

    Homepage (#19)
    * Added events
    
    * Events page created
    
    * Added more to events

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -2,7 +2,6 @@ import angular from 'angular';
 import { homeDirective } from './home.directive';
 import { navbar } from '../navbar/navbar';
 import { createEvent } from '../createEvent/createEvent';
-import { checkoutEvent } from '../checkout/checkout';
 
 export const home = angular.module( 'home', [ navbar.name, createEvent.name, checkoutEvent.name ] )
 	.config( function ( $stateProvider, $urlRouterProvider ) {
