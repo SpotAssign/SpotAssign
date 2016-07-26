b7d769853dc5e09540b7199f7b7217c90c3190e0
Cameron Nuckols <camnuckols@gmail.com>
2016-07-26 14:22:20 -0500

    Homepage (#20)
    * Added events
    
    * Events page created
    
    * Added more to events
    
    * Fixed checkout error
    
    * Update dashboard.js

--- app/components/dashboard/dashboard.js
+++ app/components/dashboard/dashboard.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
 import { dashboardDirective } from './dashboard.directive';
+import { sideNav } from '../sideNav/sideNav';
 
-export const dashboard = angular.module( 'dashboard', [] )
+export const dashboard = angular.module( 'dashboard', [ sideNav.name ] )
 .config( function ( $stateProvider, $urlRouterProvider ) {
 	$urlRouterProvider.otherwise( '/' );
 
