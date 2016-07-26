12a9e8fa09d30e4dc93c3d2fc4a1837fff5b86c1
Cameron Nuckols <camnuckols@gmail.com>
2016-07-26 14:22:20 -0500

    Homepage (#20)
    * Added events
    
    * Events page created
    
    * Added more to events
    
    * Fixed checkout error
    
    * Update dashboard.js

--- app/components/checkout/checkout.js
+++ app/components/checkout/checkout.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import { checkoutDirective } from './checkout.directive';
 import { sideNav } from '../sideNav/sideNav';
 
-export const checkoutEvent = angular.module( 'checkoutEvent', [sideNav.name] )
+export const checkoutEvent = angular.module( 'checkoutEvent', [ sideNav.name ] )
 	.config( function( $stateProvider, $urlRouterProvider ) {
 		$urlRouterProvider.otherwise( '/' );
 
