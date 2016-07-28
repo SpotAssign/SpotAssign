9928edaefc99b21c8df2c1a784ba5658ce489bfa
Cameron Nuckols <camnuckols@gmail.com>
2016-07-28 16:37:20 -0500

    Changes to event page :cookie:

--- app/components/createEvent/createEvent.directive.js
+++ app/components/createEvent/createEvent.directive.js
@@ -1,6 +1,7 @@
 import './createEvent.scss';
 import { CreateEventController as controller } from './createEvent.controller';
 import template from './createEvent.html';
+import 'materialize-css/bin/materialize.css';
 
 export const createEventDirective = () => {
 	return {
