d2baf9b10139de6710e16595d01f7e6a3a1c6c5c
rhaynes3k <rhaynes3k@gmail.com>
2016-08-02 10:26:41 -0500

    Find view (#54)
    * FindEvent styling
    
    * added search filter to html and ctrl

--- app/components/findEvent/findEvent.controller.js
+++ app/components/findEvent/findEvent.controller.js
@@ -1,2 +1,23 @@
 export class FindEventController {
 }
+$(function () {
+    $(":file").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
+
+function imageIsLoaded(e) {
+    $('#myImg').attr('src', e.target.result);
+};
+
+function findVal() {
+    $scope.findVal = service.market.getall()
+    .then(function(results) {
+        $scope.data = data;
+        console.log($scope.data);
+    })
+}
