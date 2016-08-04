7792701a47fcf210d15cab680f03ec135821870d
rhaynes3k <rhaynes3k@gmail.com>
2016-08-04 10:15:44 -0500

    Find v (#67)
    * FindEvent styling
    
    * added search filter to html and ctrl
    
    * EventFind styling

--- app/components/findEvent/findEvent.controller.js
+++ app/components/findEvent/findEvent.controller.js
@@ -1,23 +1,14 @@
-export class FindEventController {
-}
-$(function () {
-    $(":file").change(function () {
-        if (this.files && this.files[0]) {
-            var reader = new FileReader();
-            reader.onload = imageIsLoaded;
-            reader.readAsDataURL(this.files[0]);
-        }
-    });
-});
+class FindEventController {
+     constructor(  ) {
+       
+         this.markets = [
+         {title: "Provo Farmers Market", location: "Provo, UT", contact: "Old McDonald", phone: "(801) 555-5555", image: "http://billevansins.com/wp-content/uploads/2014/04/palouse-farm-country-eastern-washington.jpg", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut, beatae cumque deserunt dicta doloremque enim et fugit ipsa iure magni maxime nemo nesciunt odio perspiciatis quam qui sapiente vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, asperiores beatae cumque dicta dolorem enim error et, facilis fuga hic mollitia nam officia possimus quo quos repellat sed ut voluptatem."},
+         {title: "Dallas Farmers Market", location: "Dallas, TX", contact: "Farmer Brown", phone: "(469) 555-4444", image: "http://billevansins.com/wp-content/uploads/2014/04/palouse-farm-country-eastern-washington.jpg", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut, beatae cumque deserunt dicta doloremque enim et fugit ipsa iure magni maxime nemo nesciunt odio perspiciatis quam qui sapiente vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, asperiores beatae cumque dicta dolorem enim error et, facilis fuga hic mollitia nam officia possimus quo quos repellat sed ut voluptatem."},
+         {title: "Soulard's Market", location: "Saint Louis, MO", contact: "Maw Kettle", phone: "(314) 555-3333", image: "http://billevansins.com/wp-content/uploads/2014/04/palouse-farm-country-eastern-washington.jpg", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut, beatae cumque deserunt dicta doloremque enim et fugit ipsa iure magni maxime nemo nesciunt odio perspiciatis quam qui sapiente vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, asperiores beatae cumque dicta dolorem enim error et, facilis fuga hic mollitia nam officia possimus quo quos repellat sed ut voluptatem."}
+            ];
 
-function imageIsLoaded(e) {
-    $('#myImg').attr('src', e.target.result);
-};
+     }
+ }
 
-function findVal() {
-    $scope.findVal = service.market.getall()
-    .then(function(results) {
-        $scope.data = data;
-        console.log($scope.data);
-    })
-}
+ FindEventController.$inject= [];
+ export { FindEventController };
