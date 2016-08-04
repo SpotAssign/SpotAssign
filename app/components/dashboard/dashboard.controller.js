c05d29dd5378f749ed5c3666b7f35c571d9ef8ef
Flávio Carvalho <flavio046@hotmail.com>
2016-08-04 11:35:26 -0500

    fixed upload btn and cleared some newMap code (#69)

--- app/components/dashboard/dashboard.controller.js
+++ app/components/dashboard/dashboard.controller.js
@@ -13,24 +13,24 @@ class DashboardController {
 	// of a market/event then they are taken to the createEvent page
 	getCurrentUser() {
 		return this.service.user.getCurrentOrCreate()
-		.then( user => {
-			this.timeout(() => {
-				this.user = user;
-				if (!user.admin[0]) {
-					this.state.go('createEvent')
-				} else {
-					this.getMap();
-				}
+			.then( user => {
+				this.timeout( () => {
+					this.user = user;
+					if ( !user.admin[0] ) {
+						this.state.go( 'createEvent' );
+					} else {
+						this.getMap();
+					}
+				} );
 			} );
-		} );
 	}
 
 	getMap() {
-		return this.service.map.getOne( this.user.admin[ this.user.admin.length - 1 ].map[ this.user.admin[ this.user.admin.length - 1 ].map.length - 1 ] ).then( response => {
+		return this.service.map.getOne( this.user.admin[this.user.admin.length - 1].map[this.user.admin[this.user.admin.length - 1].map.length - 1] ).then( response => {
 			localStorage.setItem( 'map', JSON.stringify( response ) );
 			this.map = response;
-	} );
-}
+		} );
+	}
 
 }
 DashboardController.$inject = [ 'service', '$state', '$timeout', '$stateParams' ];
